6fdc9e9b3e265e52a2d09a229639841e36754a78
shuto-shinoda <shuto.88.tb@gmail.com>
2020-10-14 15:26:31 +0900

    修正

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,7 @@
 // that code so it'll be compiled.
 
 require("@rails/ujs").start()
-require("turbolinks").start()
+// require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
@@ -15,5 +15,5 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-//= require jquery
-//= require bootstrap-sprockets
+// = require jquery
+// = require bootstrap-sprockets
